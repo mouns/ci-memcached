952dbe16d9057b60dc25d9294bb21a8a234afc74
Nathanaelle <nathanaelle@adrider.fr>
2011-04-25 12:22:10 +0200

    License + README

--- libraries/Memcached.php
+++ libraries/Memcached.php
@@ -4,10 +4,48 @@
  *
  * Library for handling libmemcached pools
  *
+ * @link http://php.net/manual/en/book.memcached.php
  * @link http://en.wikipedia.org/wiki/Memcached
  * @link http://en.wikipedia.org/wiki/Memcachedb
  *
  * @author Nathanaelle <https://github.com/nathanaelle>
+ *
+ * @license combinaison of (BSD (cond 1,2,5) + Art 13 AGPL (cond 3,4) )
+ *
+ * Copyright (c) 2010-2011, MOUNS <https://github.com/mouns>
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     1 Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     2 Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     3 You have permission to link or combine any work with this work into a
+ *       single combined work, and to convey the resulting work.
+ *       The terms of this license will continue to apply to this work, but the
+ *       work with which it is combined will remain governed by its own license.
+ *     4 If you modify the source or the binary, your modified version must
+ *       prominently offer all users interacting with it remotely through a
+ *       computer network (if your version supports such interaction) an
+ *       opportunity to receive your version by providing access to the
+ *       corresponding distribution from a network server at no charge, through
+ *       some standard or customary means of facilitating copying of software.
+ *     5 Neither the name of MOUNS nor the names of its contributors may be
+ *       used to endorse or promote products derived from this software without
+ *       specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY MOUNS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL MOUNS AND CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
 class CI_Memcached	{
@@ -192,9 +230,12 @@ class CI_Memcached	{
 	 *
 	 * @param	string	$key
 	 * @param	mixed	$value
+	 * @param	integer	$expiration
 	 */
-	protected function append_or_add($key, $value) {
-		return $this->m->append( $key, $value ) or $this->m->add( $key, $value, $this->config['expiration'] ) or $this->m->append_or_add( $key, $value );
+	protected function append_or_add($key, $value, $expiration = NULL) {
+		if(!is_int($expiration))
+			$expiration = $this->config['expiration'];
+		return $this->m->append( $key, $value ) or $this->m->add( $key, $value, $expiration ) or $this->m->append_or_add( $key, $value, $expiration );
 	}
 
 
